89a39c42b1c0ddd72f43f1c102a7668bf5db610d
Michal Ščepka <55493644+michalscepka@users.noreply.github.com>
2025-02-28 15:48:48 +0100

    Add Domain layer

---
+++ src/Eshop.Domain/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Eshop.Domain.Entities;
+
+public class Product
+{
+    public required Guid Id { get; set; }
+    
+    [MaxLength(50)]
+    public required string Name { get; set; }
+    
+    public required Uri ImgUri { get; set; }
+    
+    public required decimal Price { get; set; }
+    
+    public string? Description { get; set; }
+}
