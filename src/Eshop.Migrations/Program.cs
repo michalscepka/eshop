e89fd03e2f752ce6fb0083d0d8ab91e950d59187
Michal Ščepka <55493644+michalscepka@users.noreply.github.com>
2025-03-02 17:13:21 +0100

    Use Serilog for Migrations app

--- src/Eshop.Migrations/Program.cs
+++ src/Eshop.Migrations/Program.cs
@@ -3,28 +3,48 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
 
-var builder = Host.CreateDefaultBuilder(args);
-
-builder.ConfigureAppConfiguration(config =>
+try
 {
-    var environmentName = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
+    Log.Logger = new LoggerConfiguration()
+        .WriteTo.Console(theme: AnsiConsoleTheme.Code)
+        .CreateLogger();
+
+    Log.Information("Starting up");
+    var builder = Host.CreateDefaultBuilder(args);
+
+    builder.ConfigureAppConfiguration(config =>
+    {
+        var environmentName = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
     
-    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-    config.AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true);
-});
-builder.ConfigureServices((hostBuilder, services) =>
-{
-    services.AddDbContext<EshopDbContext>(options =>
-        options.UseSqlServer(hostBuilder.Configuration.GetConnectionString("Database")));
-});
+        config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        config.AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true);
+    });
+    builder.ConfigureServices((hostBuilder, services) =>
+    {
+        services.AddDbContext<EshopDbContext>(options =>
+            options.UseSqlServer(hostBuilder.Configuration.GetConnectionString("Database")));
+    });
 
-var host = builder.Build();
+    var host = builder.Build();
 
-using (var scope = host.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<EshopDbContext>();
-    dbContext.Database.Migrate();
+    using (var scope = host.Services.CreateScope())
+    {
+        Log.Information("Applying database migrations");
+        var dbContext = scope.ServiceProvider.GetRequiredService<EshopDbContext>();
+        dbContext.Database.Migrate();
+    }
+
+    Log.Information("Migrations applied successfully");
 }
-
-Console.WriteLine("Migrations applied successfully.");
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Terminated unexpectedly");
+}
+finally
+{
+    Log.Information("Shutting down application");
+    await Log.CloseAndFlushAsync();
+}
